2015cab0225b8e33ccc1cc96a4768d3e3ea54f1c
Alex Carlson <alex@acwd.me>
2015-11-21 15:26:11 -0600

    added boat controls and physics

--- GameJamBoatThang/Assets/Scriptures/Boat.cs
+++ GameJamBoatThang/Assets/Scriptures/Boat.cs
@@ -4,28 +4,37 @@ using Rewired;
 
 public class Boat : MonoBehaviour {
 
+	public int playerIndex;
 	Player player;
-
+	
 	public float baseSpeed = 10;
-	private float maxSpeed = 20;
+	private float maxSpeed = 2.5f;
 
+	float rotation = 0;
 	float xIn;
 	float yIn;
 
+	void Start(){
+		player = ReInput.players.GetPlayer(playerIndex);
+	}
+	
 	// Update is called once per frame
 	void Update () {
 
 		xIn = player.GetAxis("MoveHorizontal");
 		yIn = player.GetAxis("MoveVertical");
-
-		if (this.GetComponent<Rigidbody> ().velocity.y < maxSpeed) {
-			this.GetComponent<Rigidbody> ().AddRelativeForce (new Vector3 (0, baseSpeed, 0));
-		} else {
-
+		
+		if (this.GetComponent<Rigidbody> ().velocity.z < maxSpeed) {
+			this.GetComponent<Rigidbody> ().AddRelativeForce (new Vector3 (0, yIn, 0));
 		}
+
+		rotation += xIn;
+
+		Quaternion temp = this.transform.localRotation;
+
+		temp.eulerAngles = new Vector3 (90, rotation, 0);
+
+		this.transform.localRotation = temp;
 	}
 
-	void MoveShip(float: dir;){
-
-	}
 }
