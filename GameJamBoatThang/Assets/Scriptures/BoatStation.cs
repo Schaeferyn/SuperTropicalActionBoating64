e62063712835650ac39f03db57775200de1ddc3a
Ryan Schaefer <RyanLSchaefer@gmail.com>
2015-11-21 17:50:51 -0600

    Pirate/wheel interactions.

--- GameJamBoatThang/Assets/Scriptures/BoatStation.cs
+++ GameJamBoatThang/Assets/Scriptures/BoatStation.cs
@@ -1,15 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Rewired;
 
-public class BoatStation : MonoBehaviour {
+public class BoatStation : MonoBehaviour
+{
+    public bool isActive = false;
+    //Pirate activePirate;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pirate")
+        {
+            Pirate p = other.GetComponent<Pirate>();
+            //activePirate = other.GetComponent<Pirate>();
+            //activePirate.EnterStationTrigger(this);
+            p.EnterStationTrigger(this);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("trigger exit");
+        if (other.tag == "Pirate")
+        {
+            Debug.Log(other.name);
+
+            //if(!isActive)
+            //{
+                Pirate p = other.GetComponent<Pirate>();
+            //if(activePirate == p)
+            //{
+            //    activePirate.ExitStationTrigger();
+            //}
+            p.ExitStationTrigger();
+            //}
+        }
+    }
+
+    public virtual void Activate(Pirate p)
+    {
+        isActive = true;
+        //activePirate = p;
+    }
+
+    public virtual void Deactivate()
+    {
+        Debug.Log(name + " deactivating at " + Time.time);
+        isActive = false;
+        //activePirate = null;
+    }
+
+    public virtual void ProcessControls(Player player)
+    {
+
+    }
 }
---
+++ GameJamBoatThang/Assets/Scriptures/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    void Awake() { instance = this; }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
