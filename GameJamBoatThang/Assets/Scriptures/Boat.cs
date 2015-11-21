bfa54b29b62a3accaeb99e9e5264d6ad235c499a
Alex Carlson <alex@acwd.me>
2015-11-21 16:02:12 -0600

    added camera lock

--- GameJamBoatThang/Assets/Scriptures/Boat.cs
+++ GameJamBoatThang/Assets/Scriptures/Boat.cs
@@ -7,7 +7,7 @@ public class Boat : MonoBehaviour {
 	public int playerIndex;
 	Player player;
 	
-	public float baseSpeed = 10;
+	public float baseSpeed = 2;
 	private float maxSpeed = 2.5f;
 
 	float rotation = 0;
@@ -23,17 +23,17 @@ public class Boat : MonoBehaviour {
 
 		xIn = player.GetAxis("MoveHorizontal");
 		yIn = player.GetAxis("MoveVertical");
-		
+
 		if (this.GetComponent<Rigidbody> ().velocity.z < maxSpeed) {
 			this.GetComponent<Rigidbody> ().AddRelativeForce (new Vector3 (0, yIn, 0));
 		}
 
 		rotation += xIn;
-
+		
 		Quaternion temp = this.transform.localRotation;
-
-		temp.eulerAngles = new Vector3 (90, rotation, 0);
-
+		
+		temp.eulerAngles = new Vector3 (90, rotation/2, 0);
+		
 		this.transform.localRotation = temp;
 	}
 
---
+++ GameJamBoatThang/Assets/lockRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lockRotation : MonoBehaviour {
+
+	// Update is called once per frame
+	void LateUpdate () {
+		GetComponent<Camera> ().transform.rotation = Quaternion.Euler (new Vector3 (90, 0, 0));
+	}
+}
