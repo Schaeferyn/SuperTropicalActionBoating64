0a64cfee3222675f4c7dbf0557041161f610776c
Alex Carlson <alex@acwd.me>
2015-11-21 13:42:47 -0600

    boat script tweaks

--- GameJamBoatThang/Assets/Scriptures/Boat.cs
+++ GameJamBoatThang/Assets/Scriptures/Boat.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Boat : MonoBehaviour {
 
+	public float baseSpeed = 10;
+	public float maxSpeed = 20;
+	private GameObject myBoat;
+
 	// Use this for initialization
 	void Start () {
-	
+		myBoat = this.transform.root;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		myBoat.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, baseSpeed/10));
 	}
 }
