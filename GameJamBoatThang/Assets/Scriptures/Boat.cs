294defb53ed0fc3521a81e4696d564a74545c369
Alex Carlson <alex@acwd.me>
2015-11-21 13:04:02 -0600

    basic ship movement

--- GameJamBoatThang/Assets/Scriptures/Boat.cs
+++ GameJamBoatThang/Assets/Scriptures/Boat.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Boat : MonoBehaviour {
 
+	public float baseSpeed = 1;
+	public float maxSpeed = 0.2f;
+	private Transform myBoat;
+
 	// Use this for initialization
 	void Start () {
-	
+		myBoat = this.transform.root;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (myBoat.GetComponent<Rigidbody> ().velocity.z < maxSpeed) {
+			myBoat.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, baseSpeed));
+		}
 	}
 }
