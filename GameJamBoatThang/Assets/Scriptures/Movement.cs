3c6ca8c710d9c169988b15057ef0712ff002dea9
Alex Carlson <alex@acwd.me>
2015-11-21 13:31:05 -0600

    upping movement script

---
+++ GameJamBoatThang/Assets/Scriptures/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	
+	public Vector2 speed = new Vector2(1,0);
+	
+	void Start(){
+	}
+	
+	void Update () {
+		
+		//GetComponentInParent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
+		
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (speed.x * inputX, speed.y * inputY, 0);
+		
+		movement *= Time.deltaTime;
+		transform.Translate (movement);
+	}
+}
