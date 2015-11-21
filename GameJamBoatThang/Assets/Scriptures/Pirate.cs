7f271ab12aceede5251cab781b174af97b337d97
Ryan Schaefer <RyanLSchaefer@gmail.com>
2015-11-21 13:02:13 -0600

    Rewired Input Manager added with simple player movement in the PirateTestScene

---
+++ GameJamBoatThang/Assets/Scriptures/Pirate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Pirate : MonoBehaviour
+{
+    public int playerIndex;
+    Player player;
+
+    Rigidbody myBody;
+
+    float xIn;
+    float yIn;
+
+    public float moveSpeed;
+    Vector3 velo = Vector3.zero;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = ReInput.players.GetPlayer(playerIndex);
+
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        xIn = player.GetAxis("MoveHorizontal");
+        yIn = player.GetAxis("MoveVertical");
+
+        velo.x = xIn * moveSpeed;
+        velo.z = yIn * moveSpeed;
+
+        myBody.velocity = velo;
+    }
+}
