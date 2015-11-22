a07d2ade46de35dd75d55180f05d9978ad1ee248
Alex Carlson <alex@acwd.me>
2015-11-22 07:04:20 -0600

    tropicop

---
+++ GameJamBoatThang/Assets/Scriptures/policeGun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class policeGun : MonoBehaviour {
+
+	GameObject[] boats;
+
+	// Use this for initialization
+	void Start () {
+		boats = GameObject.FindGameObjectsWithTag ("Boat");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < boats.Length; i++) {
+			float dist = Vector3.Distance(transform.position, boats[i].transform.position);
+			Debug.Log(dist);
+		}
+	}
+}
