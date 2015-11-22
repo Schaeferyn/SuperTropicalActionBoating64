a5a8cc4d5403abe05d25662d2fb0fd76edcf624d
Ryan Schaefer <RyanLSchaefer@gmail.com>
2015-11-21 19:17:32 -0600

    Sail station + animation.

--- GameJamBoatThang/Assets/Scriptures/SpritesheetAnimator.cs
+++ GameJamBoatThang/Assets/Scriptures/SpritesheetAnimator.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum EndState
+{
+    Loop,
+    Stop
+}
+
 public class SpritesheetAnimator : MonoBehaviour
 {
     public Sprite[] sprites;
@@ -10,26 +16,77 @@ public class SpritesheetAnimator : MonoBehaviour
     public float frameInterval = 0.25f;
     float frameTimer;
 
+    public bool startEnabled = true;
+    public EndState endState;
+    bool isPlaying = false;
+
+    public bool shouldPlayIdleSailAnimation = false;
+
 	// Use this for initialization
 	void Start ()
     {
         myRenderer = GetComponent<SpriteRenderer>();
         frameTimer = frameInterval;
+
+        if (startEnabled)
+            PlayAnim();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (!isPlaying) return;
+
         frameTimer -= Time.deltaTime;
         if(frameTimer <= 0)
         {
             if (frameIndex < sprites.Length - 1)
                 frameIndex++;
             else
-                frameIndex = 0;
+            {
+                if (endState == EndState.Loop)
+                    frameIndex = 0;
+                else
+                    AnimEnded();
+            }
 
             myRenderer.sprite = sprites[frameIndex];
             frameTimer = frameInterval;
         }
 	}
+
+    public void AnimEnded()
+    {
+        isPlaying = false;
+
+        Debug.Log("aksdjfkasdfh");
+        if(shouldPlayIdleSailAnimation)
+        {
+            transform.parent.Find("Sail").GetComponent<SpritesheetAnimator>().PlayAnim();
+            this.SetRendererVisibility(false);
+            
+        }
+    }
+
+    public void PlayAnim()
+    {
+        Debug.Log(name + " started at " + Time.time);
+
+        SetRendererVisibility(true);
+
+        myRenderer.sprite = sprites[0];
+        frameIndex = 0;
+        frameTimer = frameInterval;
+        isPlaying = true;
+    }
+
+    public void PauseAnim()
+    {
+        isPlaying = false;
+    }
+
+    public void SetRendererVisibility(bool isVisible)
+    {
+        myRenderer.enabled = isVisible;
+    }
 }
--- GameJamBoatThang/Assets/Scriptures/StationSails.cs
+++ GameJamBoatThang/Assets/Scriptures/StationSails.cs
@@ -4,6 +4,34 @@ using Rewired;
 
 public class StationSails : BoatStation
 {
+    public SpritesheetAnimator sailOpen;
+    public SpritesheetAnimator sailClosed;
+    public SpritesheetAnimator sailIdle;
+
+    public override void Activate(Pirate p)
+    {
+        base.Activate(p);
+
+        sailClosed.SetRendererVisibility(false);
+        sailIdle.SetRendererVisibility(false);
+        sailOpen.SetRendererVisibility(true);
+        sailOpen.PlayAnim();
+        //sailOpen.AnimEnded() => 
+        //{
+            
+        //}
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+
+        sailOpen.SetRendererVisibility(false);
+        sailIdle.SetRendererVisibility(false);
+        sailClosed.SetRendererVisibility(true);
+        sailClosed.PlayAnim();
+    }
+
     public override void ProcessControls(Player player)
     {
 
