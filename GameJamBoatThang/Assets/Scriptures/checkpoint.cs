ea1aa8ab4ba0706a78969b6ce42339be2f1c8eb1
Alex Carlson <alex@acwd.me>
2015-11-22 03:56:30 -0600

    victory music

--- GameJamBoatThang/Assets/Scriptures/checkpoint.cs
+++ GameJamBoatThang/Assets/Scriptures/checkpoint.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class checkpoint : MonoBehaviour {
 	
 	bool isPassed = false;
 	public bool finishLine = false;
+
+	#if UNITY_EDITOR
+
+	void Update(){
+		if (Input.anyKeyDown) {
+			YouWin();
+		}
+	}
+
+	#endif
 
 	void OnTriggerEnter(Collider col){
 
@@ -21,6 +32,18 @@ public class checkpoint : MonoBehaviour {
 		}
 	}
 	void YouWin(){
-		Debug.Log ("Game Over!  You win!");
+		AudioSource mainAudio = GameObject.Find ("BoatPartyAudio").GetComponent<AudioSource> ();
+		AudioSource victAudio = GameObject.Find ("VictoryAudio").GetComponent<AudioSource> ();
+		if (mainAudio.volume > 0) {
+			mainAudio.volume-=0.1f;
+		}
+
+		victAudio.Play ();
+
+		if (mainAudio.volume < 0.5f && victAudio.volume < 1f) {
+			victAudio.volume+=0.1f;
+		}
+
+
 	}
 }
