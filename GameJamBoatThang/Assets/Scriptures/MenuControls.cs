039e7a69f819d70128bcc9e4b98087ac8678dfb0
Alex Carlson <alex@acwd.me>
2015-11-22 06:22:42 -0600

    main menu tweaks

--- GameJamBoatThang/Assets/Scriptures/MenuControls.cs
+++ GameJamBoatThang/Assets/Scriptures/MenuControls.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
+using UnityEngine.UI;
 
 public class MenuControls : MonoBehaviour {
+
+	public int playerIndex;
+	InputDevice myDevice;
+	bool aIn;
+	public Image[] controllers;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +17,21 @@ public class MenuControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKeyDown) {
+
+		if (InputManager.Devices.Count - 1 >= playerIndex)
+			myDevice = InputManager.Devices[playerIndex];
+		else
+			myDevice = null;
+		
+		if (myDevice == null) return;
+
+		aIn = myDevice.Action1.WasPressed;
+
+		for (int i = 0; i < InputManager.Devices.Count; i++) {
+			Debug.Log(controllers[i]);
+		}
+
+		if (aIn) {
 			Launch ();
 		}
 	}
