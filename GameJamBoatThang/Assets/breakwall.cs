b556d312ef650c6ecde9125598b5211f9f5cdfa2
Alex Carlson <alex@acwd.me>
2015-11-21 21:32:30 -0600

    worked some on breakable walls

---
+++ GameJamBoatThang/Assets/breakwall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class breakwall : MonoBehaviour {
+
+	int health = 2;
+
+	void Update(){
+		if (health <= 0) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "projectile"){
+			health--;
+		}
+	}
+}
